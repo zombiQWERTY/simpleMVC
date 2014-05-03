592e35875fe83210d7491cb09d675f9aa9960312
zombiQWERTY <zombiqwerty@yandex.ru>
2014-05-03 23:44:45 +0400

    Убрана константа SITE

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 header('Content-Type: text/html; charset=utf-8');
 setlocale(LC_ALL, 'Russian_Russia.65001');
 
-define('SITE', true);
 define('BASE_URL', 'http://'.$_SERVER['HTTP_HOST']);
 define('APPPATH', '/application/');
 define('SYSPATH', '/system/');
